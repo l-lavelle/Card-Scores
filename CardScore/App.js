8ec9e6a7bc5718080a649edee36f4713e55918c0
Lauren <lauren.m.lavelle@wmich.edu>
2023-12-31 12:38:53 -0500

    fix folder strucutre and break up component files

--- CardScore/App.js
+++ CardScore/App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, Image, ScrollView } from "react-native";
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { styles } from "./AppStyle";
+import { SpadesPlayers } from "./components/Spades/SpadesPlayers/SpadesPlayers";
 
 function HomeScreen({ navigation }) {
   return (
@@ -21,20 +22,24 @@ function HomeScreen({ navigation }) {
             ever in your favor.
           </Text>
           <View style={styles.gameContainer}>
-            <View style={styles.individualGameContainer}>
-              <Image
-                style={styles.gameImg}
-                source={require("./assets/Spades.png")}
-              />
-              <View style={styles.individualGame}>
-                <Text style={styles.individualGameInfoHeader}>Spades</Text>
-                <Text style={styles.individualGameInfo}># of Players: 4</Text>
-                <Text style={styles.individualGameInfo}>
-                  Game: Win at least the number of tricks bid between you and
-                  your partner
-                </Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Spades Players")}
+            >
+              <View style={styles.individualGameContainer}>
+                <Image
+                  style={styles.gameImg}
+                  source={require("./assets/Spades.png")}
+                />
+                <View style={styles.individualGame}>
+                  <Text style={styles.individualGameInfoHeader}>Spades</Text>
+                  <Text style={styles.individualGameInfo}># of Players: 4</Text>
+                  <Text style={styles.individualGameInfo}>
+                    Game: Win at least the number of tricks bid between you and
+                    your partner
+                  </Text>
+                </View>
               </View>
-            </View>
+            </TouchableOpacity>
             <View style={styles.individualGameContainer}>
               <Image
                 style={styles.gameImg}
@@ -62,27 +67,11 @@ function HomeScreen({ navigation }) {
                 </Text>
               </View>
             </View>
-            <Text onPress={() => navigation.navigate("Choose Players")}></Text>
             <StatusBar style="auto" />
           </View>
         </View>
       </ScrollView>
     </>
-  );
-}
-
-function PlayersScreen() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.header}>Players</Text>
-      </View>
-      <View style={styles.container}>
-        <Text>Card Scorer</Text>
-        <Text>Hi my name is lauren</Text>
-        <StatusBar style="auto" />
-      </View>
-    </View>
   );
 }
 
@@ -93,7 +82,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Choose Players" component={PlayersScreen} />
+        <Stack.Screen name="Spades Players" component={SpadesPlayers} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -105,3 +94,5 @@ export default App;
 // When game loads choose players: depending on how many you can have
 // Insturction incons for how to score and rules
 // Scoring
+
+// Need to change font family: open sans and others
---
+++ CardScore/components/Spades/SpadesPlayers/SpadesPlayers.js
@@ -0,0 +1,17 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+
+export function SpadesPlayers() {
+  return (
+    <View>
+      <View>
+        <Text>Players</Text>
+      </View>
+      <View>
+        <Text>Card Scorer</Text>
+        <Text>Hi my name is lauren</Text>
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
